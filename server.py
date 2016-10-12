e6e86465889c7f11593114108e269571d87ee51c
Codecool-Palko <matolaypal@gmail.com>
2016-10-12 12:44:26 +0200

    store the boards in centraldb

--- server.py
+++ server.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request
+import json
+from model import *
+from playhouse.shortcuts import model_to_dict, dict_to_model
+
+
 app = Flask(__name__)
 
 
@@ -8,6 +13,13 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/', methods=['POST'])
+def post_data_to_database():
+    board_dict = json.loads(request.form['board'])
+    board_model = dict_to_model(Board, board_dict)
+    Board.create(title=board_model.title, body=board_model.body)
+    return 'model created'
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
