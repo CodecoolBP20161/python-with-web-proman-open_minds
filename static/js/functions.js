01a3d8307d5e4be81a4ab8ec68456716ebafa75a
ppayter-cc <pomsarpeter@gmail.com>
2016-10-13 16:57:07 +0200

    html code cleaning

--- static/js/functions.js
+++ static/js/functions.js
@@ -3,8 +3,8 @@
 function displayBoard(boardObject) {
 
     var divBoard = $('<div class="board" id=board_'+ boardObject.id +'></div>');
-    // append 'view board' buttons
-    var showCard = $('<button id="cards" class="btn btn-primary btn-xs showcardsbutton" data-toggle="tooltip" title="view cards on this board"><i class="fa fa-list-alt" aria-hidden="true"></i> view cards</button>')
+    // adds 'view board' buttons
+    var showCard = $('<button id="cards" class="btn btn-primary btn-xs showcardsbutton" title="view cards on this board"><i class="fa fa-list-alt" aria-hidden="true"></i> view cards</button>')
     divBoard.append(showCard)
     divBoard.appendTo('#board-container');
     showCard.on('click', function(){
@@ -12,8 +12,8 @@ function displayBoard(boardObject) {
         $('#card-container').show();
         state.runCardPage(boardObject.id);
     });
-    // append 'delete' buttons
-    var btnDelete = $('<button class="btn btn-default btn-xs delboardbutton" data-toggle="tooltip" title="delete this board and all cards on it"><i class="fa fa-trash" aria-hidden="true"></i></button>')
+    // adds 'delete' buttons
+    var btnDelete = $('<button class="btn btn-default btn-xs delboardbutton" title="delete this board and all cards on it"><i class="fa fa-trash" aria-hidden="true"></i></button>')
     var state = new State(new DataBaseImp());
     btnDelete.on('click', function(){
 
@@ -22,7 +22,7 @@ function displayBoard(boardObject) {
         board.hide();
     });
     divBoard.append(btnDelete)
-    // append board title & description
+    // adds board title & description
     divBoard.append("<p class='titletext'> <strong>"+ boardObject.title +"</strong> </p>");
     divBoard.append("<p>"+ boardObject.body +" </p>");
 }
@@ -30,8 +30,8 @@ function displayBoard(boardObject) {
 
 function displayCard(cardObject) {
     var divCard = $('<div class="card" id=card_'+ cardObject.id +'></div>');
-    // append 'delete' buttons
-    var btnDelete = $('<button class="btn btn-default btn-xs delcardbutton" data-toggle="tooltip" title="delete this card"><i class="fa fa-trash" aria-hidden="true"></i></button>')
+    // adds 'delete' buttons
+    var btnDelete = $('<button class="btn btn-default btn-xs delcardbutton" title="delete this card"><i class="fa fa-trash" aria-hidden="true"></i></button>')
     btnDelete.on('click', function(){
         var state = new State(new DataBaseImp());
 
@@ -40,7 +40,7 @@ function displayCard(cardObject) {
         card.hide();
     });
     divCard.append(btnDelete)
-    // append card title & description
+    // adds card title & description
     divCard.append("<p class='titletext'><strong>"+ cardObject.title +"</strong></p>");
     divCard.append("<p>"+ cardObject.body +" </p>");
     divCard.appendTo('#card-container');
