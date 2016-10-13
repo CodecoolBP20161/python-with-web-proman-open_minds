a9182df93f9a36a3a90a72dba0b4af38c2cb7988
Codecool-Palko <matolaypal@gmail.com>
2016-10-13 13:54:53 +0200

    create init_db function; reorganize connect, drop_tables, create_tables methods;

--- build.py
+++ build.py
@@ -1,11 +1,20 @@
 from model import *
-
 # Configure your database connection here
 # database name = should be your username on your laptop
-# database user = should be your username on your laptop
-# db = PostgresqlDatabase('', user='')
 
-db.connect()
 
-db.drop_tables([Board, Card], safe=True, cascade=True)
-db.create_tables([Board, Card], safe=True)
+def init_db():
+    try:
+        with open('db.ini') as f:
+            db_name = f.read()
+    except FileNotFoundError:
+        with open('db.ini', 'w') as f:
+            db_name = f.write(input("What's name your database?\n"))
+    return db_name
+
+db = PostgresqlDatabase(init_db())
+
+if __name__ == '__main__':
+    db.connect()
+    db.drop_tables([Board, Card], safe=True, cascade=True)
+    db.create_tables([Board, Card], safe=True)
